6c83afaf0b979c594940995fb293b6c29e8ca0a0
dev-bong <qhddnjswo011@gmail.com>
2022-11-18 13:16:49 +0900

    ✨ router 기본 구성 및 게시글 조회 화면 구성

---
+++ src/routes/Home.svelte
@@ -0,0 +1,18 @@
+<script>
+    import fastapi from "../lib/api"
+    let post_list = []
+  
+    function get_post_list() {
+        fastapi('get', '/api/post/list', {}, (json) => {
+            post_list = json
+        })
+    }
+  
+    get_post_list()
+  </script>
+  
+  <ul>
+    {#each post_list as post}
+      <li>{post.subject}</li>
+    {/each}
+  </ul>
